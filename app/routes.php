b6368e955e847e1f2abb6a4f166673a25ae3f3e7
joe <josepho011235@gmail.com>
2014-03-01 08:21:24 -0500

    merged local Laravel directory.

---
+++ app/routes.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+
+Route::get('/pizza', function()
+{
+	return View::make("other");
+});
+
+
+
+Route::get('/hamburger', function()
+{
+	return '<html><h1>I love hamburgers too <\h1><\html>';
+});
+
+Route::get('user/{id}', function($id)
+{
+    return 'User '.$id;
+});
+
+Route::get('/login', function()
+{
+	return View::make('forms.userForm');
+});
+
+
+
+Route::post('/login', function()
+{
+	return'User has been logged in';
+});
+
+Route::get('/test', function(){
+	return View::make('test');
+});
+
---
+++ app/views/master.blade.php
@@ -0,0 +1,13 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Narwhals</title>
+</head>
+<body>
+    @include('header')
+    <p>Why, the Narhwal surely bacons at midnight, my good sir!</p>
+    @include('footer')
+</body>
+</html>
